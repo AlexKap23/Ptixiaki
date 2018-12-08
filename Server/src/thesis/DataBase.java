13f70148aeb9474306b176c269747e4ee8c03586
Alexandros Kapniaris <alex.kapniaris@gmail.com>
2018-12-08 14:40:53 +0200

    firebase on server

---
+++ Server/src/thesis/DataBase.java
@@ -0,0 +1,22 @@
+
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public interface DataBase {
+	
+	//creates the data base if it has not been created earlier
+	public void createDb(); 
+	
+	//inserts values that came from the temperature sensor into the table that holds temp values
+	abstract void insertTemp(double value,String date,String time); 
+	
+	//inserts  values that came from the motion sensor into the table that holds motion values
+	abstract void insertMotion(String motion,String date,String time);
+	
+	//method that runs a query into the database
+	abstract String fetchFromBd(String query);
+	
+}
